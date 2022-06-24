14fc8cfb69edca19b6fa1bb280db6a03893af29c
apexdev <mwangi.em37@gmail.com>
2022-06-24 11:01:55 +0300

    Updated GET /questions request url
    Work modified in the frontend, in QuestionView.js script.

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -22,7 +22,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `http://127.0.0.1:5000/api/v1/questions?page=${this.state.page}`, //TODO: update request URL [COMPLETED]
       type: 'GET',
       success: (result) => {
         this.setState({

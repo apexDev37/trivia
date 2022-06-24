a83875a417726264623696d8175f7864d0eb1f48
apexdev <mwangi.em37@gmail.com>
2022-06-24 15:40:04 +0300

    Added success and error tests for GET categories
    Work modified in the backend, in the test_flaskr.py script.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -34,6 +34,63 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_retrieve_all_categories_from_db(self):
+        """
+        Test that API method returns a response
+        with all the categories in the database.
+        """
+
+        # Given
+        endpoint = '/api/v1/categories'
+
+        # When
+        response = self.client().get(endpoint)
+        data = json.loads(response.data)
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertGreater(len(data['categories']), 0)
+
+    # def test_404_when_no_categories_present_in_db(self):
+    #     """
+    #     Test that API endpoint returns a 404 error
+    #     response when no categories are present in the database.
+    #     """
+
+    #     # Given
+    #     endpoint = '/api/v1/categories'
+
+    #     # When
+    #     response = self.client().get(endpoint)
+    #     data = json.loads(response.data)
+
+    #     # Then
+    #     self.assertEqual(response.status_code, 404)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertNotIn(data['categories'], data)
+    #     self.assertGreater(data['message'], 'requested resource not found')
+
+    def test_405_when_request_method_is_not_GET_on_categories_endpoint(self):
+        """
+        Test that API method returns a 405 error
+        response when GET is not the request method on
+        the API endpoint: '/api/v1/categories'.
+        """
+
+        # Given
+        endpoint = '/api/v1/categories'
+
+        # When
+        response = self.client().post(endpoint)    
+        data = json.loads(response.data)
+
+        # Then
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertNotIn('categories', data, 'categories key should not be in data')
+        self.assertEqual(data['message'], 'method not allowed')
 
 
 

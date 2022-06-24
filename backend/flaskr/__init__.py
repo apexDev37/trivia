652e220793b532288f062ab05773c97b89c542bc
apexdev <mwangi.em37@gmail.com>
2022-06-24 12:14:55 +0300

    Added API request, error handlers section
    Work modified in the backend, in the __init__.py script.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -144,5 +144,47 @@ def create_app(test_config=None):
     including 404 and 422.
     """
 
-    return app
+    # --- App Request, Error Handlers
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'bad request'
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'requested resource not found'
+        }), 404
+    
+    @app.errorhandler(405)
+    def methhod_not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'method not allowed'
+        }), 405
+
+    @app.errorhandler(422)
+    def unporcessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable entity'
+        }), 422
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'internal server error'
+        }), 500
+    
+    
+    return app

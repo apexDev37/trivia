75597f500d11dabc0a3973c11c6bebf9f1c145e8
apexdev <mwangi.em37@gmail.com>
2022-06-25 21:24:14 +0300

    Completed TODOs: Updated server API endpoints

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -49,7 +49,7 @@ class QuizView extends Component {
     }
 
     $.ajax({
-      url: '/quizzes', //TODO: update request URL
+      url: `http://127.0.0.1:5000/api/v1/quizzes`, //TODO: update request URL [COMPLETED]
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',

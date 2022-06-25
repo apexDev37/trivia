3b96f45d75888c709bcd40e2621290f1ff91e796
apexdev <mwangi.em37@gmail.com>
2022-06-26 01:26:55 +0300

    Updates in models

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+# refactor db path
 database_name = 'trivia'
 database_path = 'postgresql://postgres:postgres-admin@{}/{}'.format('localhost:5432', database_name)
 
